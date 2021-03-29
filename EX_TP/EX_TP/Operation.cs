3e0bf3ce279c5a0d54ac4afe568433659e097371
Anas Fartas <80294911+FartasAnas@users.noreply.github.com>
2021-03-29 22:59:59 +0100

    Add files via upload

---
+++ EX_TP/EX_TP/Operation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_TP
+{
+    class Operation
+    {
+        private static int cptop=0;
+        private readonly int numop;
+        private readonly DateTime dateop;
+        private readonly MAD montant;
+        private readonly string libelle;
+        public Operation(string libelle,MAD montant)
+        {
+            this.numop = ++(Operation.cptop);
+            this.dateop = DateTime.Now;
+            this.montant = montant;
+            this.libelle = libelle;
+        }
+        public void afficherOP()
+        {
+            Console.WriteLine("         "+this.libelle + ":" + this.dateop + "|| N°" + this.numop + "||"  + this.montant.afficherMAD());
+        }
+
+    }
+}
